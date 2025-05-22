e0811b232cc28fe392ad6cabce9157f113f4b709
Victor Silva <victorsilvaluiz10@gmail.com>
2025-05-22 08:56:00 -0300

    Add files via upload

---
+++ atividades de python/atividade_7.py
@@ -0,0 +1,10 @@
+print('---------------------')
+num0 = int(input('DIGITE O VALOR DE N1 :'))
+print('---------------------')
+num1 = int(input('DIGITE O VALOR DE N2 : ')) 
+#--------------------------------------------------------------
+print(f'O VALOR DE N1 É {num1} ')
+print(f'O VALOR DE N2 É {num0} ')
+
+
+print('\n' * 6)
