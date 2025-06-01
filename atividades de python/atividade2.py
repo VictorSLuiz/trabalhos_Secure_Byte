256d0e23dce9d8ed7a051ce4f054f89b0b3589d4
Bruno7sx <bruninho073hv@gmail.com>
2025-06-01 14:58:02 -0300

    Add files via upload

---
+++ atividades de python/atividade2.py
@@ -0,0 +1,6 @@
+
+celsius = float(input("Digite a temperatura em Celsius: "))
+                
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"A temperatura de {celsius}°C é equivalente a {fahrenheit}°F.")
