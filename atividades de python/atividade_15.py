c0f2a0138e2272bdf5a074d7812c83cc7f3480b6
Victor Silva <victorsilvaluiz10@gmail.com>
2025-06-09 07:52:10 -0300

    Update atividade_15.py

--- atividades de python/atividade_15.py
+++ atividades de python/atividade_15.py
@@ -1,11 +1,11 @@
-valororiginal = float(input("Digite o valor do produto (em R$):"))
+valor_original = float(input("Digite o valor do produto (em R$):"))
 
-if valororiginal > 100:
-    desconto = valororiginal * 0.10 
-    valor_com_desconto = valororiginal - desconto 
+if valor_original > 100:
+    desconto = valor_original * 0.10 
+    valor_com_desconto = valor_original - desconto 
     print(f"Voce recebeu um desconto de R$ {desconto:.2f}.")
     print(f"Valor com desconto: R$ {valor_com_desconto:.2f}")
 else:
     print("Sem desconto aplicado.")
-    print(f"Valor final: R$ {valororiginal:.2f}")
+    print(f"Valor final: R$ {valor_original:.2f}")
 
