675e2141dcbad58b3faed97ebc9fa72b2b5bc822
Victor Silva <victorsilvaluiz10@gmail.com>
2025-06-09 07:49:34 -0300

    Update atividade_13.py

--- atividades de python/atividade_13.py
+++ atividades de python/atividade_13.py
@@ -4,9 +4,9 @@ print('DIGITE UM NÚMERO :')
 n1 = int(input()) 
 
 if n1 % 3 == 0 and n1 % 5 == 0 :
-    valor = ('é divisível')
+    condicao = ('é divisível')
 else :
-    valor = ('não é divisível')
+    condicao = ('não é divisível')
 
-print(f'{n1} {valor} por 3 e por 5 ')
+print(f'{n1} {condicao} por 3 e por 5 ')
 
