4f601f94a2bdf0c6b5e368f251965c5e70f86e6e
Victor Silva <victorsilvaluiz10@gmail.com>
2025-08-12 08:24:37 -0300

    Add files via upload

---
+++ VIctor/script.js
@@ -0,0 +1,4 @@
+function mudarMessagem() {
+    const mensagem = document.getElementById("mensagem");
+    mensagem.innerHTML = "você acabou de ganhar 5% porcento de descontos na nossa loja! <br> aproveite";
+}
