4a98556045544e8ccb128f65ea9c099aa95da255
Victor Silva <victorsilvaluiz10@gmail.com>
2025-08-31 20:16:39 -0300

    Remover arquivos desnecessários e adicionar novos arquivos de código para a funcionalidade de login e página de cupom

--- VIctor/script.js
+++
@@ -1,4 +0,0 @@
-function mudarMessagem() {
-    const mensagem = document.getElementById("mensagem");
-    mensagem.innerHTML = "você acabou de ganhar 5% porcento de descontos na nossa loja! <br> aproveite";
-}
